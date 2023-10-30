d9e16839c854f71345a4baec851098358741e2c3
Guillaume <Guillaume>
2023-10-30 23:42:11 +0100

    Switch: added ternary option

--- dist/harmony-ui.browser.js
+++ dist/harmony-ui.browser.js
@@ -1650,12 +1650,13 @@ class HTMLHarmonySwitchElement extends HTMLElement {
 	#htmlSwitchOuter;
 	#htmlSwitchInner;
 	#state = false;
-	constructor(options) {
+	#ternary = false;
+	constructor() {
 		super();
 		this.#doOnce = true;
-		this.#htmlLabel = createElement('div', {class: 'harmony-switch-label'});
-		this.#htmlSwitchOuter = createElement('span', {class: 'harmony-switch-outer'});
-		this.#htmlSwitchInner = createElement('span', {class: 'harmony-switch-inner'});
+		this.#htmlLabel = createElement('div', { class: 'harmony-switch-label' });
+		this.#htmlSwitchOuter = createElement('span', { class: 'harmony-switch-outer' });
+		this.#htmlSwitchInner = createElement('span', { class: 'harmony-switch-inner' });
 		this.addEventListener('click', () => this.toggle());
 	}
 
@@ -1664,6 +1665,7 @@ class HTMLHarmonySwitchElement extends HTMLElement {
 			this.append(this.#htmlLabel);
 			this.append(this.#htmlSwitchOuter);
 			this.#htmlSwitchOuter.append(this.#htmlSwitchInner);
+			this.#refresh();
 			this.#doOnce = false;
 		}
 	}
@@ -1680,7 +1682,7 @@ class HTMLHarmonySwitchElement extends HTMLElement {
 	set state(state) {
 		if (this.#state != state) {
 			this.#state = state;
-			this.dispatchEvent(new CustomEvent('change', {detail:{state:state, value:state}}));
+			this.dispatchEvent(new CustomEvent('change', { detail: { state: state, value: state } }));
 		} else {
 			this.#state = state;
 		}
@@ -1699,13 +1701,39 @@ class HTMLHarmonySwitchElement extends HTMLElement {
 		return this.#state;
 	}
 
+	set ternary(ternary) {
+		console.log(ternary);
+		this.#ternary = ternary;
+		this.#refresh();
+	}
+
+	get ternary() {
+		return this.#ternary;
+	}
+
 	toggle() {
-		this.state = !this.#state;
+		if (this.#ternary) {
+			if (this.#state === false) {
+				this.#state = undefined;
+			} else if (this.#state === undefined) {
+				this.#state = true;
+			} else {
+				this.#state = false;
+			}
+		} else {
+			this.state = !this.#state;
+		}
 		this.#refresh();
 	}
 
 	#refresh() {
-		this.classList[this.#state ? 'add' : 'remove']('on');
+		this.classList.remove('on');
+		this.classList.remove('off');
+		this.classList[this.#ternary ? 'add' : 'remove']('ternary');
+		if (this.#state === undefined) {
+			return;
+		}
+		this.classList.add(this.#state ? 'on' : 'off');
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
@@ -1722,11 +1750,14 @@ class HTMLHarmonySwitchElement extends HTMLElement {
 			case 'disabled':
 				this.disabled = newValue;
 				break;
+			case 'ternary':
+				this.ternary = true;
+				break;
 		}
 	}
 
 	static get observedAttributes() {
-		return ['data-label', 'data-i18n', 'disabled'];
+		return ['data-label', 'data-i18n', 'disabled', 'ternary'];
 	}
 }
 
--- src/elements/harmony-switch.js
+++ src/elements/harmony-switch.js
@@ -1,4 +1,4 @@
-import {createElement, hide, show, display} from '../harmony-html.js';
+import { createElement } from '../harmony-html.js';
 
 export class HTMLHarmonySwitchElement extends HTMLElement {
 	#doOnce;
@@ -7,12 +7,13 @@ export class HTMLHarmonySwitchElement extends HTMLElement {
 	#htmlSwitchOuter;
 	#htmlSwitchInner;
 	#state = false;
-	constructor(options) {
+	#ternary = false;
+	constructor() {
 		super();
 		this.#doOnce = true;
-		this.#htmlLabel = createElement('div', {class: 'harmony-switch-label'});
-		this.#htmlSwitchOuter = createElement('span', {class: 'harmony-switch-outer'});
-		this.#htmlSwitchInner = createElement('span', {class: 'harmony-switch-inner'});
+		this.#htmlLabel = createElement('div', { class: 'harmony-switch-label' });
+		this.#htmlSwitchOuter = createElement('span', { class: 'harmony-switch-outer' });
+		this.#htmlSwitchInner = createElement('span', { class: 'harmony-switch-inner' });
 		this.addEventListener('click', () => this.toggle());
 	}
 
@@ -21,6 +22,7 @@ export class HTMLHarmonySwitchElement extends HTMLElement {
 			this.append(this.#htmlLabel);
 			this.append(this.#htmlSwitchOuter);
 			this.#htmlSwitchOuter.append(this.#htmlSwitchInner);
+			this.#refresh();
 			this.#doOnce = false;
 		}
 	}
@@ -37,7 +39,7 @@ export class HTMLHarmonySwitchElement extends HTMLElement {
 	set state(state) {
 		if (this.#state != state) {
 			this.#state = state;
-			this.dispatchEvent(new CustomEvent('change', {detail:{state:state, value:state}}));
+			this.dispatchEvent(new CustomEvent('change', { detail: { state: state, value: state } }));
 		} else {
 			this.#state = state;
 		}
@@ -56,13 +58,39 @@ export class HTMLHarmonySwitchElement extends HTMLElement {
 		return this.#state;
 	}
 
+	set ternary(ternary) {
+		console.log(ternary);
+		this.#ternary = ternary;
+		this.#refresh();
+	}
+
+	get ternary() {
+		return this.#ternary;
+	}
+
 	toggle() {
-		this.state = !this.#state;
+		if (this.#ternary) {
+			if (this.#state === false) {
+				this.#state = undefined;
+			} else if (this.#state === undefined) {
+				this.#state = true;
+			} else {
+				this.#state = false;
+			}
+		} else {
+			this.state = !this.#state;
+		}
 		this.#refresh();
 	}
 
 	#refresh() {
-		this.classList[this.#state ? 'add' : 'remove']('on');
+		this.classList.remove('on');
+		this.classList.remove('off');
+		this.classList[this.#ternary ? 'add' : 'remove']('ternary');
+		if (this.#state === undefined) {
+			return;
+		}
+		this.classList.add(this.#state ? 'on' : 'off');
 	}
 
 	attributeChangedCallback(name, oldValue, newValue) {
@@ -79,10 +107,13 @@ export class HTMLHarmonySwitchElement extends HTMLElement {
 			case 'disabled':
 				this.disabled = newValue;
 				break;
+			case 'ternary':
+				this.ternary = true;
+				break;
 		}
 	}
 
 	static get observedAttributes() {
-		return ['data-label', 'data-i18n', 'disabled'];
+		return ['data-label', 'data-i18n', 'disabled', 'ternary'];
 	}
 }
