852330a23243ecf7e556ac3dd17bc34b4b048a04
Guillaume <Guillaume>
2023-11-21 18:04:54 +0100

    harmony-label-property: added gap between property and value

--- dist/define/browser/harmony-label-property.js
+++ dist/define/browser/harmony-label-property.js
@@ -1,6 +1,6 @@
 import {HTMLHarmonyLabelPropertyElement, styleInject} from '../../harmony-ui.browser.js';
 import {InjectUiStyle} from './.inject-ui-style.js';
 if (window.customElements) {
-	styleInject(``);
+	styleInject(`:root{--harmony-label-property-gap:0.5rem}harmony-label-property{display:flex;gap:var(--harmony-label-property-gap)}`);
 	customElements.define('harmony-label-property', HTMLHarmonyLabelPropertyElement);
 }
