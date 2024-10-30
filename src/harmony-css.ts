03159f350d1681bfe7c50a7a09f20e81963889f3
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-30 23:56:04 +0100

    Added typescript declarations

--- src/harmony-css.ts
+++ src/harmony-css.ts
@@ -1,18 +1,18 @@
-export async function documentStyle(cssText) {
+export async function documentStyle(cssText: string) {
 	return await shadowRootStyle(document, cssText);
 }
 
-export function documentStyleSync(cssText) {
+export function documentStyleSync(cssText: string) {
 	return shadowRootStyleSync(document, cssText);
 }
 
-export async function shadowRootStyle(shadowRoot, cssText) {
+export async function shadowRootStyle(shadowRoot: Document | ShadowRoot, cssText: string) {
 	const sheet = new CSSStyleSheet;
 	await sheet.replace(cssText);
 	shadowRoot.adoptedStyleSheets.push(sheet);
 }
 
-export function shadowRootStyleSync(shadowRoot, cssText) {
+export function shadowRootStyleSync(shadowRoot: Document | ShadowRoot, cssText: string) {
 	const sheet = new CSSStyleSheet;
 	sheet.replaceSync(cssText);
 	shadowRoot.adoptedStyleSheets.push(sheet);
