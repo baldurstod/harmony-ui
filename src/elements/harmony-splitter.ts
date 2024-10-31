043bd967dfe9c7cc3741f78a6e09974e463fcd96
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-31 23:54:53 +0100

    Added typescript declarations

--- src/elements/harmony-splitter.ts
+++ src/elements/harmony-splitter.ts
@@ -1,30 +1,25 @@
-import { createElement, hide } from '../harmony-html.js';
+import { createElement, hide } from '../harmony-html';
 import splitterCSS from '../css/harmony-splitter.css';
-import { shadowRootStyleSync } from '../harmony-css.js';
+import { shadowRootStyleSync } from '../harmony-css';
 
 export class HTMLHarmonySplitterElement extends HTMLElement {
 	#shadowRoot;
-	#htmlPanel1;
-	#htmlPanel2;
-	#htmlGutter;
+	#htmlPanel1: HTMLSlotElement;
+	#htmlPanel2: HTMLSlotElement;
+	#htmlGutter: HTMLElement;
 	#doOnce = true;
-	#orientation;
+	#orientation: string = 'v';
 	#split = 0.5;
-	#startOffsetLeft;
-	#startOffsetTop;
-	#startPageX;
-	#startPageY;
-	#startOffsetX;
-	#startOffsetY;
+	#startOffsetLeft = 0;
+	#startOffsetTop = 0;
+	#startPageX = 0;
+	#startPageY = 0;
+	#startOffsetX = 0;
+	#startOffsetY = 0;
 	#dragging = false;
 
-	constructor(options) {
+	constructor() {
 		super();
-		this.#initHtml();
-		//this.#doOnceOptions = options;
-	}
-
-	#initHtml() {
 		this.#shadowRoot = this.attachShadow({ mode: 'closed' });
 		shadowRootStyleSync(this.#shadowRoot, splitterCSS);// sync version is used to ensure style is loaded before computation occurs
 
@@ -32,19 +27,19 @@ export class HTMLHarmonySplitterElement extends HTMLElement {
 			class: 'panel',
 			name: '1',
 			parent: this.#shadowRoot,
-		});
+		}) as HTMLSlotElement;
 		this.#htmlGutter = createElement('div', {
 			class: 'gutter',
 			parent: this.#shadowRoot,
 			/*events: {
 				mousedown: event => this.#handleMouseDown(event),
 			},*/
-		});
+		}) as HTMLElement;
 		this.#htmlPanel2 = createElement('slot', {
 			class: 'panel',
 			name: '2',
 			parent: this.#shadowRoot,
-		});
+		}) as HTMLSlotElement;
 
 		this.#htmlGutter.addEventListener('mousedown', event => this.#handleMouseDown(event));
 		document.body.addEventListener('mousemove', event => this.#handleMouseMove(event), { capture: true });
@@ -64,7 +59,7 @@ export class HTMLHarmonySplitterElement extends HTMLElement {
 		this.#htmlPanel2.style.flexBasis = (1 - this.#split) * 100 + '%';
 	}
 
-	setOrientation(orientation) {
+	setOrientation(orientation: string) {
 		this.classList.remove('vertical', 'horizontal');
 		switch (orientation) {
 			case 'v':
@@ -80,7 +75,7 @@ export class HTMLHarmonySplitterElement extends HTMLElement {
 		}
 	}
 
-	#handleMouseDown(event) {
+	#handleMouseDown(event: MouseEvent) {
 		this.#startOffsetLeft = this.#htmlGutter.offsetLeft;
 		this.#startOffsetTop = this.#htmlGutter.offsetTop;
 		this.#startOffsetX = event.offsetX;
@@ -91,7 +86,7 @@ export class HTMLHarmonySplitterElement extends HTMLElement {
 		event.stopPropagation();
 	}
 
-	#handleMouseMove(event) {
+	#handleMouseMove(event: MouseEvent) {
 		if (!this.#dragging) {
 			return;
 		}
@@ -112,7 +107,7 @@ export class HTMLHarmonySplitterElement extends HTMLElement {
 		this.#update();
 	}
 
-	attributeChangedCallback(name, oldValue, newValue) {
+	attributeChangedCallback(name: string, oldValue: string, newValue: string) {
 		switch (name) {
 			case 'orientation':
 				this.setOrientation(newValue);
