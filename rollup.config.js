dfd35e20bd620564ed3577d947224a14cb60b66f
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-02-02 14:11:10 +0100

    Removed .elements.js Removed dependencies cssnano and postcss Cleaned rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,4 @@
 import fs from 'fs';
-import postcss from 'postcss';
-import cssnano from 'cssnano';
-import { elements } from './src/elements/.elements.js';
 import image from '@rollup/plugin-image';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
@@ -9,71 +6,6 @@ import css from 'rollup-plugin-import-css';
 import child_process from 'child_process';
 
 const TEMP_BUILD = './dist/dts/index.js';
-
-async function writeElement(elementName, elementClass, injectCSS, isBrowser = false) {
-	let cssPath = `./src/css/${elementName}.css`;
-	let input = fs.readFileSync(cssPath);
-	let css = await postcss([cssnano()]).process(input, { from: undefined, });
-
-	let fileContent = `import {${elementClass}, styleInject} from '${isBrowser ? '../../harmony-ui.browser.js' : '../index.js'}';
-import {InjectUiStyle} from './.inject-ui-style.js';
-if (window.customElements) {
-${injectCSS ? `	styleInject(\`${css}\`);` : ''}
-	customElements.define('${elementName}', ${elementClass});
-}`;
-
-	fs.writeFile(`./dist/define/${isBrowser ? 'browser/' : ''}${elementName}.js`, Buffer.from(fileContent), async (err) => { if (err) throw err });
-}
-
-async function writeGlobal(isBrowser = false) {
-	let cssPath = `./src/css/harmony-ui.css`;
-	let input = fs.readFileSync(cssPath);
-	let css = await postcss([cssnano()]).process(input, { from: undefined, });
-
-	let fileContent = `import {styleInject} from '${isBrowser ? '../../harmony-ui.browser.js' : '../index.js'}';
-export const InjectUiStyle = (function () {
-	styleInject(\`${css}\`);
-}());`;
-
-	fs.writeFile(`./dist/define/${isBrowser ? 'browser/' : ''}.inject-ui-style.js`, Buffer.from(fileContent), async (err) => { if (err) throw err });
-}
-/*
-async function writeDefines(isBrowser = false) {
-	let fileContent = `import { styleInject } from '${isBrowser ? '../../harmony-ui.browser.js' : '../index.js'}';`;
-	for (const element of elements) {
-		let cssPath = `./src/css/${element.name}.css`;
-		let input = fs.readFileSync(cssPath);
-		let css = await postcss([cssnano()]).process(input, { from: undefined, });
-		const name = element.name.replaceAll('-', '');
-		fileContent += `
-import { ${element.class} } from '${isBrowser ? '../../harmony-ui.browser.js' : '../index.js'}';
-let defined${name} = false;
-export function define${name}() {
-	if (window.customElements && !defined${name}) {
-${element.injectCSS ? `		styleInject(\`${css}\`);` : ''}
-		customElements.define('${element.name}', ${element.class});
-		defined${name} = true;
-	}
-}
-`;
-	}
-
-	fs.writeFile(`./dist/define/${isBrowser ? 'browser/' : ''}defines.js`, Buffer.from(fileContent), async (err) => { if (err) throw err });
-}*/
-
-//fs.mkdirSync('./dist/define/', { recursive: true });
-//fs.mkdirSync('./dist/define/browser/', { recursive: true });
-
-//writeElements(false);
-//writeElements(true);
-
-function writeElements(isBrowser) {
-	for (const element of elements) {
-		writeElement(element.name, element.class, false, isBrowser);
-	}
-	writeGlobal(isBrowser);
-	//writeDefines(isBrowser);
-}
 
 export default [
 	{
